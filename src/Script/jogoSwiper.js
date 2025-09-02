7c683770e189d63370eafeca12d2d8cf8e97c195
Dheiko <fabriciogarcia386@gmail.com>
2025-09-02 20:30:33 -0300

    main do jogo feita

--- src/Script/jogoSwiper.js
+++ src/Script/jogoSwiper.js
@@ -1,11 +1,20 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
+var swiper = new Swiper(".jogo-side-image", {
+    spaceBetween: 16,
     slidesPerView: 4,
     freeMode: true,
+    loop: true,
     watchSlidesProgress: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
   });
-  var swiper2 = new Swiper(".mySwiper2", {
+  var swiper2 = new Swiper(".jogo-main-image", {
     spaceBetween: 10,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
