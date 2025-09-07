a7764b8799a313072548bf8fb1bc1e04b1cfa418
0Gabriel0 <gabrielgol2007@gmail.com>
2025-09-07 16:13:01 -0300

    Revert "Tentando pegar as informações do banco de dados"
    This reverts commit e2d4c0f3a25743c945259538ce7e1da707ce0cb9.

--- src/back-end/settings/bd.sql
+++ src/back-end/settings/bd.sql
@@ -1,163 +1,103 @@
--- Primeiro sempre apaga antes de criar
-DROP DATABASE IF EXISTS PhantomGames;
+create database PhantomGames;
 
-CREATE DATABASE PhantomGames;
+drop database PhantomGames;
+ 
+use PhantomGames;
+ 
+/*tabela usuario */
+create table usuario(
+ID_usuario int(6) primary key not null auto_increment, 
+Nome varchar(80) not null,
+email varchar(50) not null,
+senha varchar(20) not null,
+classe varchar(1) not null);
+ 
+/*tabela categoria e categoria_jogos */
+create table categoria(
+ID_categoria int(2) primary key not null auto_increment, 
+Nome varchar(20) not null);
+ 
+/*tabela genero e genero_jogos */
+create table genero(
+ID_genero int(2) primary key not null auto_increment, 
+Nome varchar(20) not null);
 
-USE PhantomGames;
+/*tabela jogos */
+create table jogos(
+ID_jogo int(5) primary key not null auto_increment, 
+ID_categoria int(2) not null, 
+ID_genero int(2) not null, 
+Nome_jogo varchar(20) not null,
+Preco_jogo decimal(5,2) not null,
+Logo_jogo text not null,
+Descricao_jogo text,
+Capa_jogo text not null,
+Midias_jogo text not null,
+Faixa_etaria text not null,
+Media_nota decimal(10,2) not null,
+foreign key (ID_categoria) REFERENCES categoria(ID_categoria),
+foreign key (ID_genero) REFERENCES genero(ID_genero)
+);
 
--- tabela usuario
-CREATE TABLE
-    usuario (
-        ID_usuario INT (6) PRIMARY KEY AUTO_INCREMENT,
-        Nome VARCHAR(80) NOT NULL,
-        email VARCHAR(50) NOT NULL UNIQUE,
-        senha VARCHAR(20) NOT NULL,
-        classe CHAR(1) NOT NULL
-    );
 
--- tabela categoria
-CREATE TABLE
-    categoria (
-        ID_categoria INT PRIMARY KEY AUTO_INCREMENT,
-        Nome_categoria VARCHAR(50) NOT NULL
-    );
+create table categoria_jogos(
+ID_categoria_jogos int(2) primary key not null auto_increment, 
+ID_categoria int(2),
+ID_jogo int(5),
+foreign key (ID_categoria) REFERENCES categoria(ID_categoria),
+foreign key (ID_jogo) REFERENCES jogos(ID_jogo));
+ 
+create table genero_jogos(
+ID_genero_jogos int(2) primary key not null auto_increment, 
+ID_genero int(2),
+ID_jogo int(5),
+foreign key (ID_genero) REFERENCES genero(ID_genero),
+foreign key (ID_jogo) REFERENCES jogos(ID_jogo));
+ 
+/*tabela lista_desejos */
+create table lista_desejos(
+ID_lista_desejos int(5) primary key not null auto_increment, 
+ID_usuario int(6),
+ID_jogo int(5),
+foreign key (ID_usuario) references usuario(ID_usuario),
+foreign key (ID_jogo) references jogos(ID_jogo));
+ 
+/*tabela biblioteca */
+create table biblioteca(
+ID_biblioteca int(5) primary key not null auto_increment, 
+ID_usuario int(6),
+ID_jogo int(5),
+foreign key (ID_usuario) references usuario(ID_usuario),
+foreign key (ID_jogo) references jogos(ID_jogo));
+ 
+/*tabela carrinho */
+create table carrinho(
+ID_carrinho int(5) primary key not null auto_increment, 
+forma_pagamento varchar(20) not null,
+data_compra date not null,
+ID_usuario int(6),
+ID_jogo int(5),
+foreign key (ID_usuario) references usuario(ID_usuario),
+foreign key (ID_jogo) references jogos(ID_jogo));
+ 
+/*tabela comentário */
+create table comentario(
+ID_comentario int(12) primary key not null auto_increment, 
+ID_usuario int(6),
+ID_jogo int(5),
+txtcomentario varchar(255),
+data_comentario date not null,
+nota int(1),
+foreign key (ID_usuario) references usuario(ID_usuario),
+foreign key (ID_jogo) references jogos(ID_jogo));
 
--- tabela genero
-CREATE TABLE
-    genero (
-        ID_genero INT PRIMARY KEY AUTO_INCREMENT,
-        Nome_genero VARCHAR(50) NOT NULL
-    );
+INSERT INTO genero (Nome)
+VALUES ('Ação'), ('Aventura'), ('Casual'), ('Corrida'), ('Esportes'), ('Estratégia'), ('Indie'), ('Luta'), ('Musical'), ('Narrativo'), 
+('Plataforma'), ('Puzzle'), ('RPG'), ('Simulação'), ('Sobrevivência'), ('Terror'), ('Tiro');
 
--- tabela jogos
-CREATE TABLE
-    jogos (
-        ID_jogo INT PRIMARY KEY AUTO_INCREMENT,
-        ID_categoria INT NOT NULL,
-        ID_genero INT NOT NULL,
-        Nome_jogo VARCHAR(100) NOT NULL,
-        Descricao_jogo TEXT,
-        Preco_jogo DECIMAL(10, 2) NOT NULL,
-        Logo_jogo TEXT NOT NULL,
-        Capa_jogo TEXT NOT NULL,
-        Midias_jogo TEXT NOT NULL,
-        Faixa_etaria VARCHAR(20) NOT NULL,
-        Media_nota DECIMAL(3, 2) DEFAULT 0,
-        FOREIGN KEY (ID_categoria) REFERENCES categoria (ID_categoria),
-        FOREIGN KEY (ID_genero) REFERENCES genero (ID_genero)
-    );
+INSERT INTO categoria (Nome)
+VALUES ('Singleplayer'), ('Multiplayer Local'), ('Multiplayer Online'), ('Co-op'), ('PvP'), ('PvE'), ('MMO'), ('Cross-Plataform'),
+('2D'), ('3D'), ('2.5D'), ('Top-Down'), ('Side-Scrooling'), ('Isométrico'), ('Primeira Pessoa'), ('Terceira Pessoa'),
+('Linear'), ('Mundo Aberto'), ('Sandbox'), ('Campanha'), ('Missões/Fases'), ('Permadeath'), ('Rouguelike');
 
--- tabela categoria_jogos (relacionamento N:N)
-CREATE TABLE
-    categoria_jogos (
-        ID_categoria_jogos INT PRIMARY KEY AUTO_INCREMENT,
-        ID_categoria INT,
-        ID_jogo INT,
-        FOREIGN KEY (ID_categoria) REFERENCES categoria (ID_categoria),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
 
--- tabela genero_jogos (relacionamento N:N)
-CREATE TABLE
-    genero_jogos (
-        ID_genero_jogos INT PRIMARY KEY AUTO_INCREMENT,
-        ID_genero INT,
-        ID_jogo INT,
-        FOREIGN KEY (ID_genero) REFERENCES genero (ID_genero),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
-
--- tabela lista_desejos
-CREATE TABLE
-    lista_desejos (
-        ID_lista_desejos INT PRIMARY KEY AUTO_INCREMENT,
-        ID_usuario INT,
-        ID_jogo INT,
-        FOREIGN KEY (ID_usuario) REFERENCES usuario (ID_usuario),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
-
--- tabela biblioteca
-CREATE TABLE
-    biblioteca (
-        ID_biblioteca INT PRIMARY KEY AUTO_INCREMENT,
-        ID_usuario INT,
-        ID_jogo INT,
-        FOREIGN KEY (ID_usuario) REFERENCES usuario (ID_usuario),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
-
--- tabela carrinho
-CREATE TABLE
-    carrinho (
-        ID_carrinho INT PRIMARY KEY AUTO_INCREMENT,
-        forma_pagamento VARCHAR(20) NOT NULL,
-        data_compra DATE NOT NULL,
-        ID_usuario INT,
-        ID_jogo INT,
-        FOREIGN KEY (ID_usuario) REFERENCES usuario (ID_usuario),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
-
--- tabela comentario
-CREATE TABLE
-    comentario (
-        ID_comentario INT PRIMARY KEY AUTO_INCREMENT,
-        ID_usuario INT,
-        ID_jogo INT,
-        txtcomentario VARCHAR(255),
-        data_comentario DATE NOT NULL,
-        nota TINYINT CHECK (nota BETWEEN 0 AND 10),
-        FOREIGN KEY (ID_usuario) REFERENCES usuario (ID_usuario),
-        FOREIGN KEY (ID_jogo) REFERENCES jogos (ID_jogo)
-    );
-
--- Inserindo categorias
-INSERT INTO
-    categoria (Nome_categoria)
-VALUES
-    ('Singleplayer'),
-    ('Multiplayer Local'),
-    ('Multiplayer Online'),
-    ('Co-op'),
-    ('PvP'),
-    ('PvE'),
-    ('MMO'),
-    ('Cross-Plataform'),
-    ('2D'),
-    ('3D'),
-    ('2.5D'),
-    ('Top-Down'),
-    ('Side-Scrolling'),
-    ('Isométrico'),
-    ('Primeira Pessoa'),
-    ('Terceira Pessoa'),
-    ('Linear'),
-    ('Mundo Aberto'),
-    ('Sandbox'),
-    ('Campanha'),
-    ('Missões/Fases'),
-    ('Permadeath'),
-    ('Roguelike');
-    
--- Inserindo gêneros
-INSERT INTO
-    genero (Nome_genero)
-VALUES
-    ('Ação'),
-    ('Aventura'),
-    ('Casual'),
-    ('Corrida'),
-    ('Esportes'),
-    ('Estratégia'),
-    ('Indie'),
-    ('Luta'),
-    ('Musical'),
-    ('Narrativo'),
-    ('Plataforma'),
-    ('Puzzle'),
-    ('RPG'),
-    ('Simulação'),
-    ('Sobrevivência'),
-    ('Terror'),
-    ('Tiro');
